32c95cf660f4ca3b2c94c44dea7fc62687944cc6
DheerajSamanta <114645642+DheerajSamanta@users.noreply.github.com>
2023-08-13 15:58:16 +0530

    Create NewInvertedfullPyramid.c

---
+++ Pattern-Problems/NewInvertedfullPyramid.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void invertedFullPyramid();
+
+int main(){
+  invertedFullPyramid();
+  return 0;
+}
+
+void invertedFullPyramid(){
+  int num;
+  printf("Enter the number of rows:");
+  scanf("%d",&num);
+  for( int i=num;i>=1;i--){
+    for(int spaces=num-i;spaces>=0;spaces--){
+      printf(" ");
+    }
+    for(int j=i;j>=1;j--){
+       printf("* ");
+    }
+printf("\n");
+
+  }
+
+}
